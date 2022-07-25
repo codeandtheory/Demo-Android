1289c62414671d9f16199270d7601b5e0af5a953
Edwin Pradeep <edwin.pradeep@ymedialabs.com>
2022-07-25 13:38:13 +0530

    Sonar-Scanner

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-export SONAR_SCANNER_VERSION=4.7.0.2747
-export SONAR_SCANNER_HOME=$Workspace/sonar-scanner-$SONAR_SCANNER_VERSION-macosx
-export PATH=$SONAR_SCANNER_HOME/bin:$PATH
-export SONAR_SCANNER_OPTS="-server"
-#Sonar-Scanner runner
-sh /Users/runner/sonar-scanner-4.7.0.2747-macosx/bin/sonar-scanner
+pwd
+ls -lrt
