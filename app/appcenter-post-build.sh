418995c787ace232d969549216a5ab178c6c6efb
Edwin Pradeep <edwin.pradeep@ymedialabs.com>
2022-07-25 13:59:03 +0530

    Sonar-Scanner

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+
 #Sonar-Scanner runner
 sh /Users/runner/work/1/s/app/sonar-scanner-4.7.0.2747-macosx/bin/sonar-scanner
--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,3 +1,1 @@
 #!/usr/bin/env bash
-
-sh /Users/runner/work/1/s/app/sonar-scanner-4.7.0.2747-macosx/bin/sonar-scanner
